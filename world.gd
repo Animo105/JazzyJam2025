5566fe7f3edf56f0d6b76d201c0e15cb03acd354
perceval001 <156114624+perceval001@users.noreply.github.com>
2025-11-16 12:54:35 -0500

    tutorial

--- world.gd
+++ world.gd
@@ -15,3 +15,32 @@ func _on_tutoriel_message_body_entered(body: Node3D) -> void:
 func _on_tutoriel_message_body_exited(body: Node3D) -> void:
 	if body is Player:
 		Global.message.emit("")
+
+
+func _on_tutoriel_message_2_body_entered(body: Node3D) -> void:
+	if body is Player:
+		Global.message.emit("Appuyer sur ESPACE pour sauter")
+
+
+func _on_tutoriel_message_2_body_exited(body: Node3D) -> void:
+	if body is Player:
+		Global.message.emit("")
+
+
+func _on_tutoriel_message_3_body_entered(body: Node3D) -> void:
+	if body is Player:
+		Global.message.emit("Maintenir CTRL pour s'accroupir")
+
+
+func _on_tutoriel_message_3_body_exited(body: Node3D) -> void:
+	if body is Player:
+		Global.message.emit("")
+
+
+func _on_tutoriel_message_4_body_entered(body: Node3D) -> void:
+	if body is Player:
+		Global.message.emit("Appuyer sur CLIQUE GAUCHE pour ouvrire la lampe")
+
+func _on_tutoriel_message_4_body_exited(body: Node3D) -> void:
+	if body is Player:
+		Global.message.emit("")

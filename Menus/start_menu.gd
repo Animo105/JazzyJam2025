a27e4cf4a6cfa953d81d4b3fadcc085436215d1a
Animo105 <emeric2005leclerc@gmail.com>
2025-11-16 09:54:52 -0500

    Music de chase + power outage

--- Menus/start_menu.gd
+++ Menus/start_menu.gd
@@ -2,13 +2,12 @@ extends Control
 
 
 @onready var audio_stream_player: AudioStreamPlayer = $Menu/Help/AudioStreamPlayer
-@onready var menu_music: AudioStreamPlayer = $"menu music"
 
 func _ready() -> void:
-	menu_music.autoplay = true
-	menu_music.play()
+	MusicPanel.plays_menu_music(true)
 
 func _on_start_pressed() -> void:
+	MusicPanel.plays_menu_music(false)
 	get_tree().change_scene_to_file("res://world.tscn")
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	
